efef51bda5c395602fec80606fa6c17a9bea4e0f
Adam Patni <adampatni27@gmail.com>
2020-02-10 01:47:02 -0500

    added software section

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def adam_main():
     return render_template("index_2.html")
 
 
-@app.route('/1')
-def miniport_main():
-    return render_template("index.html")
-
-
 if __name__ == '__main__':
     app.run()
